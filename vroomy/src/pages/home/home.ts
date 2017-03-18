ff84f01ab23292797172d1b191ea6841d9831a43
Kyle Zhou <kylezhou2002@hotmail.com>
2017-03-18 12:06:24 -0400

    Adding current acceleration test to home

--- vroomy/src/pages/home/home.ts
+++ vroomy/src/pages/home/home.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+declare var navigator : any;
+
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	currentAccel: number;
 
-  }
+    constructor(public navCtrl: NavController) {
+    	navigator.accelerometer.getCurrentAcceleration(
+    		(acceleration) => {
+    			this.currentAccel = acceleration;
+    		},
+    		() => {
+    			this.currentAccel = NaN;
+    		},
+    	);
+    }
 
 }
